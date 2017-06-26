97580ba52188442a6da8cc6225d69b493d7a2b72
ParadoxSpiral <ParadoxSpiral@riseup.net>
2017-06-26 19:26:41 +0200

    Custom reply to VERSION/SOURCE, handle more Commands

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -15,15 +15,51 @@
 // You should have received a copy of the GNU General Public License
 // along with Parabot.  If not, see <http://www.gnu.org/licenses/>.
 
-use irc::client::server::IrcServer;
-use irc::proto::message::Message;
+use irc::client::prelude::*;
 use slog::Logger;
 
 use super::config::Server;
-use super::errors::*;
 
 const COMMAND_MODIFIER: &'static str = ".";
+// https://tools.ietf.org/html/rfc2812#section-1.3
+const CHANNEL_PREFIXES: &'static [char] = &['#', '&', '+', '!'];
 
 pub fn handle(cfg: &Server, srv: &IrcServer, log: &Logger, msg: Message) {
-    debug!(log, "Received message: {:?}", msg);
+    match msg.command {
+        // All the mostly uninteresting information
+        Command::NOTICE(..) |
+        Command::Response(Response::RPL_WELCOME, ..) |
+        Command::Response(Response::RPL_YOURHOST, ..) |
+        Command::Response(Response::RPL_CREATED, ..) |
+        Command::Response(Response::RPL_MYINFO, ..) |
+        Command::Response(Response::RPL_ISUPPORT, ..) |
+        Command::Response(Response::RPL_LUSERCLIENT, ..) |
+        Command::Response(Response::RPL_LUSEROP, ..) |
+        Command::Response(Response::RPL_LUSERUNKNOWN, ..) |
+        Command::Response(Response::RPL_LUSERCHANNELS, ..) |
+        Command::Response(Response::RPL_LUSERME, ..) |
+        Command::Response(Response::RPL_MOTDSTART, ..) |
+        Command::Response(Response::RPL_MOTD, ..) |
+        Command::Response(Response::RPL_ENDOFMOTD, ..) |
+        Command::Response(Response::ERR_NOTREGISTERED, ..) |
+        Command::Response(Response::RPL_NAMREPLY, ..) |
+        Command::Response(Response::RPL_ENDOFNAMES, ..) |
+        Command::Response(Response::RPL_TOPIC, ..) |
+        Command::Join(..) => trace!(log, "{:?}", msg),
+        Command::Raw(ref s, ..) if s == "MODE" || s == "265" || s == "266" => {
+            trace!(log, "{:?}", msg)
+        }
+        Command::PRIVMSG(target, content) => {
+            debug!(log, "private message to {}: {}", target, content);
+
+            // Test if this msg was sent to a channel. When replying, 
+            // we want to use NOTICE in that case
+            let first_char = target.chars().nth(1).unwrap();
+            let chan_msg = CHANNEL_PREFIXES.iter().any(|p| &first_char == p);
+            trace!(log, "channel message: {}", chan_msg);
+        }
+        _ => {
+            warn!(log, "Unhandled message: {:?}", msg);
+        }
+    }
 }
